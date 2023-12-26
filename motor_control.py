5f8a4de228242ded777da143319ab2f9bbf87ecf
dBerriff <berriff@icloud.com>
2023-12-26 20:38:14 +0000

    develop motor control for 2 motors and asyncio coding (push-button control)

--- motor_control.py
+++ motor_control.py
@@ -1,118 +1,102 @@
 # motor_control.py
 """ run dc motor(s) under PWM control """
+
 import asyncio
-from machine import Pin, PWM   
+from collections import namedtuple
 from time import sleep
+from l298n import L298N
 
 
-class L298nChannel:
-    """ L298N H-bridge channel
-        - states: 'F': forward, 'R': reverse, 'S': stop
-    """
-
-    def __init__(self, pwm_pin, motor_pins_, frequency, name=''):
-        self.enable = PWM(Pin(pwm_pin))
-        self.sw_1 = Pin(motor_pins_[0], Pin.OUT)
-        self.sw_2 = Pin(motor_pins_[1], Pin.OUT)
-        self.set_freq(frequency)
-        self.name = name
-        self.state = None
-        self.dc_u16 = 0
-        self.set_state('S')
-
-    def set_freq(self, frequency):
-        """ set pulse frequency """
-        if 500 < frequency <= 1500:
-            self.enable.freq(frequency)
-        else:
-            self.enable.freq(500)
-
-    def set_state(self, state):
-        """ set H-bridge switch states """
-        if state == 'F':
-            self.state = 'F'
-            self.sw_1.value(1)
-            self.sw_2.value(0)
-        elif state == 'R':
-            self.state = 'R'
-            self.sw_1.value(0)
-            self.sw_2.value(1)
-        else:  # stop!
-            self.state = 'S'
-            self.sw_1.value(1)
-            self.sw_2.value(1)
-
-    def set_dc_percent(self, percent):
-        """ set duty cycle by percent """
-        percent = max(0, percent)
-        percent = min(100, percent)
-        self.dc_u16 = 0xffff * percent // 100
-        self.enable.duty_u16(self.dc_u16)
-
-    def set_logic_off(self):
-        """ set all logic output off """
-        self.set_dc_percent(0)
-        self.sw_1.value(0)
-        self.sw_2.value(0)
-        self.state = 'S'
-
-
-class L298N:
-    """ control a generic L298N H-bridge board
-    """
-
-    def __init__(self, pwm_pins_, motor_pins_, frequency):
-        print('Initialise L298N')
-        print(f'Channel A pins: {pwm_pins_[0]}, ({motor_pins_[0]}, {motor_pins_[1]})')
-        self.channel_a = L298nChannel(
-            pwm_pins_[0], (motor_pins_[0], motor_pins_[1]), frequency, 'A')
-        print(f'Channel B pins: {pwm_pins_[1]}, ({motor_pins_[2]}, {motor_pins_[3]})')
-        self.channel_b = L298nChannel(
-            pwm_pins_[1], (motor_pins_[2], motor_pins_[3]), frequency, 'B')
-
-    
 class Motor:
     """ control speed and direction of dc motor """
     
+    Speed = namedtuple('Speed', ['f', 'r'])  # forward, reverse percentages
+    
     def __init__(self, channel, name):
         self.channel = channel
-        self.name = name
+        self.name = name  # for print or logging
+        self.state = None
+        self.speed_pc = None
 
-    def rotate(self, direction, percent):
-        """ set to direction at percent duty cycle """
-        self.channel.set_state(direction)
-        self.channel.set_dc_percent(percent)
+    def rotate(self, dc_u16):
+        """ rotate motor in self.state direction at u16 duty cycle """
+        self.channel.set_state(self.state)
+        self.channel.set_dc_u16(dc_u16)
 
-    def run(self, direction, percent):
+    def run_pc(self, percent):
         """ run the motor in the set direction at percent duty-cycle """
-        print(f'Run motor {self.name} {direction} {percent}')
-        # avoid instant change of direction
-        if direction != self.channel.direction and self.channel.dc_pc > 0:
-            self.rotate(self.channel.direction, 0)
-            sleep(0.5)
-        self.rotate(direction, percent)
+        print(f'Motor {self.name}: {self.state} {percent}%')
+        self.rotate(0xffff * percent // 100)
+        self.speed_pc = percent
+    
+    async def accel(self, target_pc):
+        """ accelerate from stop to target speed over 10 steps """ 
+        step = target_pc // 10
+        for speed in range(0, target_pc, step):
+            self.run_pc(speed)
+            await asyncio.sleep_ms(500)  # period // 10
+        self.run_pc(target_pc)
 
-    def stop(self):
-        """ stop the motor """
-        self.rotate('S', 0)
+    async def decel(self):
+        """ decelerate from current speed to stop over 5000 ms """
+        step = self.speed_pc // 10
+        for speed in range(self.speed_pc, 0, -step):
+            self.run_pc(speed)
+            await asyncio.sleep_ms(500)  # period // 10
+        await self.halt()
 
-pwm_pins = (2, 3)
-motor_pins = (4, 5, 6, 7)
-f = 500
-controller = L298N(pwm_pins, motor_pins, f)
-motor_a = Motor(controller.channel_a, 'A')
-motor_b = Motor(controller.channel_b, 'B')
-sleep(2)
-motor_a.run('F', 50)    
-sleep(5)
-motor_a.run('R', 25)  
-sleep(5)
-motor_a.stop()
-motor_b.run('R', 50)  
-sleep(5)
-motor_b.run('F', 25)
-sleep(5)
-motor_b.stop()
-motor_a.channel.set_logic_off()
-motor_b.channel.set_logic_off()
+    async def halt(self):
+        """ set speed to 0 but retain state """
+        self.run_pc(0)
+        # allow some time to halt
+        await asyncio.sleep_ms(500)
 
+    async def stop(self):
+        """ set state to 'S', halt the motor """
+        self.state = 'S'
+        await self.halt()
+
+    def set_logic_off(self):
+        """ turn off channel logic """
+        self.channel.set_logic_off()
+
+async def main():
+    """ basic test of motor control """
+    # a PWM slice comprises consecutive even and odd pins
+    # slice pins share the same frequency
+    pwm_pins = (2, 3)  # consecutive even & odd pins
+    motor_pins = (4, 5, 6, 7)
+    pulse_f = 15000  # adjust for physical motor and controller
+    controller = L298N(pwm_pins, motor_pins, pulse_f)
+    motor_a = Motor(controller.channel_a, name='A')
+    motor_b = Motor(controller.channel_b, name='B')
+    # establish initial state
+    await motor_a.stop()
+    await motor_b.stop()
+    
+    motor_a_speed = Motor.Speed(f=75, r=50)
+    motor_b_speed = Motor.Speed(f=75, r=50)
+    for _ in range(4):
+        if motor_a.state != 'F':
+            motor_a.state = 'F'
+            motor_b.state = 'R'
+            asyncio.create_task(motor_a.accel(motor_a_speed.f))
+            asyncio.create_task(motor_b.accel(motor_b_speed.r))
+        else:
+            motor_a.state = 'R'
+            motor_b.state = 'F'
+            asyncio.create_task(motor_a.accel(motor_a_speed.r))
+            asyncio.create_task(motor_b.accel(motor_b_speed.f))
+        await asyncio.sleep_ms(30_000)
+        asyncio.create_task(motor_a.decel())
+        asyncio.create_task(motor_b.decel())
+        await asyncio.sleep_ms(10_000)
+
+    motor_a.set_logic_off()
+
+if __name__ == '__main__':
+    try:
+        asyncio.run(main())
+    finally:
+        asyncio.new_event_loop()  # clear retained state
+        print('execution complete')
