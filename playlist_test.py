140a877c9f8ce18277b08853005383fed61344b4
dBerriff <berriff@icloud.com>
2024-05-11 11:11:02 +0100

    Update playlist_test.py

--- playlist_test.py
+++ playlist_test.py
@@ -10,11 +10,6 @@ from playlist_player import PlPlayer
 async def main():
     """ test playlist player controller """
 
-    def build_player(tx_p, rx_p, btn_pins_):
-        """ build player from components """
-        cmd_handler = DfpMini(tx_p, rx_p)
-        return PlPlayer(cmd_handler, btn_pins_)
-
     # pins
     # UART
     tx_pin = 16
@@ -28,8 +23,7 @@ async def main():
 
     # play_pin, v_dec_pin, v_inc_pin
     btn_pins = {"play": 20, "v_dec": 21, "v_inc": 22}
-
-    player = build_player(tx_pin, rx_pin, btn_pins)
+    player = PlPlayer(DfpMini(tx_p, rx_p), btn_pins)
     print(f'Player name: {player.name}')
     await player.reset()
     player.build_playlist(shuffled=False)
